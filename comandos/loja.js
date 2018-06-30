0b4b6e0459c6012cfe3453d7afb41a3a606da57d
uSilky <39656396+uSilky@users.noreply.github.com>
2018-06-30 15:43:42 -0300

    Update loja.js

--- comandos/loja.js
+++ comandos/loja.js
@@ -2,6 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (client, message, args) =>{
 
+   if(message.channel.id != "460886091369807872") return message.reply(`você não pode executar comandos aqui!`).then(msg => msg.delete(5000)); 
    message.delete().catch(O_o=>{})
     const loja = new Discord.RichEmbed()
        .setColor("0cff00")
