16d8eed1cf9d48a38da571dee2e5d02421506ce9
uSilky <39656396+uSilky@users.noreply.github.com>
2018-06-30 15:44:15 -0300

    Update ip.js

--- comandos/ip.js
+++ comandos/ip.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (client, message, args) =>{
 
-if(message.channel.id != "461552920941559818") return message.reply(`você não pode executar comandos aqui!`).then(msg => msg.delete(5000));
+if(message.channel.id != "460886091369807872") return message.reply(`você não pode executar comandos aqui!`).then(msg => msg.delete(5000));
    message.delete().catch(O_o=>{})
     const ip = new Discord.RichEmbed()
        .setColor("0cff00")
