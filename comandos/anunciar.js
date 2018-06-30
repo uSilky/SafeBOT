70df6b153f0f5cfcbe055e762d844720936cb3af
uSilky <39656396+uSilky@users.noreply.github.com>
2018-06-30 17:30:02 -0300

    Add files via upload

---
+++ comandos/anunciar.js
@@ -0,0 +1,24 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (client, message, args) =>{
+
+   message.delete().catch(O_o=>{})
+   if (message.member.hasPermission('MANAGE_GUILD')) {  
+               
+    let mensg = args.join(" ");
+    if(!mensg)
+       return message.channel.send("Digite a msg do aviso!")
+       
+    const anuncio = new Discord.RichEmbed()
+       .setColor("0cff00")
+       .setAuthor("Anúncio", "https://cdn.discordapp.com/emojis/460264772869554176.gif")
+       
+       .setDescription(mensg)
+       
+       .setTimestamp()
+       .setFooter(`Anunciado por: ${message.author.tag}`, message.author.avatarURL)
+    
+    message.channel.send("@everyone", anuncio)
+
+}
+}
